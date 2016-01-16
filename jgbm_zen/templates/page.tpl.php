8520b28b7e010f0bcb0bfebbf7b44997c473255e
openpaul <paulsocial@gmx.de>
2016-01-16 12:22:32 +0100

    active trail, logo

--- jgbm_zen/templates/page.tpl.php
+++ jgbm_zen/templates/page.tpl.php
@@ -13,7 +13,7 @@
     <?php if ($logo): ?>
     <?php
     	// maybe display some stadgruppen logo
-   
+   		print_r(menu_get_active_trail());
 
     ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
